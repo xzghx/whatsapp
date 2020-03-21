0096d86516da3e128e4cbb7bf9ec2823dc733c69
zahra <ghafoory.zahra@gmail.com>
2020-03-21 16:27:03 +0430

    add paginate method

--- lib/sqfliteProvider/product_provider.dart
+++ lib/sqfliteProvider/product_provider.dart
@@ -13,11 +13,37 @@ class ProductProvider extends Provider {
   }
 
   Future<bool> insertAll(List<Product> lstProducts) async {
+    /*
+//this is bad way  according to https://flutter-academy.com/async-in-flutter-advanced-futures-api/
+        Future.forEach(lstProducts, (product) async {
+      await db.insert(product);
+    });
+    */
+
     //we don't use foreach because it's not Future
     //but map is of type Future
     //and we need it because the work being done will take time
     await Future.wait(lstProducts.map((product) async {
       await this.insert(product);
     }));
+    return true;
+  }
+
+  Future<List<Product>> paginate(int page, {int limit: 8}) async {
+    //read each page from db and return
+    List<Map<String, dynamic>> maps = await db.query(tableName,
+        columns: ["id", "user_id", "title", "body"],
+        limit: limit,
+        offset: page == 1 ? 0 : ((page - 1) * limit));
+
+    if (maps.length > 0) {
+      List<Product> lst = [];
+      maps.forEach((product) {
+        if (product != null) lst.add(new Product.fromJson(product));
+      });
+
+      return lst;
+    }
+    return null;
   }
 }
