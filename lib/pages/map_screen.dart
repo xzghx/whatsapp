7b54fa1f97dd92fd25e68466635ffa1e329c1ae5
zahra <ghafoory.zahra@gmail.com>
2020-03-25 12:43:45 +0430

    animate camera to iran

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -8,22 +8,39 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(36.314,52.093),
-    zoom:5,
+    target: LatLng(10.12, 80.12),
+    zoom: 15,
   );
+  GoogleMapController _mapController;
+
+  void _googleMapController(GoogleMapController controller) {
+    //this callback function gives us a  GoogleMapController which
+    //then we can use to control map from other parts of code
+    _mapController = controller;
+    //it's not needed to pass setState() because onMapCreated is someHowe a setState
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(title:Text("map") ,),
-        body: GoogleMap(
-          mapType: MapType.hybrid,
-          initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
-//        _controller.complete(controller);
-          },
+        appBar: AppBar(
+          title: Text("map"),
         ),
-
-    );
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: _googleMapController,
+            ),
+            RaisedButton(
+              onPressed: () {
+                _mapController.animateCamera(CameraUpdate.newCameraPosition(
+                    CameraPosition(target: LatLng(36.314, 52.093))));
+              },
+              child: Text("♥Go To Iran♥"),
+            )
+          ],
+        ));
   }
 }
