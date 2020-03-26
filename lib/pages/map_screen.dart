00622430e0ee309b110702ee551f79c924584d39
zahra <ghafoory.zahra@gmail.com>
2020-03-26 09:08:58 +0430

    add google map with marker

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -7,17 +7,32 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(10.12, 80.12),
-    zoom: 15,
-  );
   GoogleMapController _mapController;
+  MapType _currentMapType;
+  Set<Marker> _markers = <Marker>{};
+
+  static final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(10.12, 80.12),
+    zoom: 8,
+  );
 
   void _googleMapController(GoogleMapController controller) {
     //this callback function gives us a  GoogleMapController which
     //then we can use to control map from other parts of code
     _mapController = controller;
     //it's not needed to pass setState() because onMapCreated is someHowe a setState
+  }
+
+  void _onMapTypeButtonPressed() {
+    /*Way 1*/
+    if (_currentMapType == MapType.normal) {
+      setState(() {
+        _currentMapType = MapType.satellite;
+      });
+    } else
+      setState(() {
+        _currentMapType = MapType.normal;
+      });
   }
 
   @override
@@ -30,18 +45,65 @@ class _MapScreenState extends State<MapScreen> {
           alignment: Alignment.bottomCenter,
           children: <Widget>[
             GoogleMap(
-              mapType: MapType.hybrid,
-              initialCameraPosition: _kGooglePlex,
+              markers: _markers,
+              mapType: _currentMapType,
+              initialCameraPosition: _initialPosition,
               onMapCreated: _googleMapController,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              onTap: _myAddMarker,
             ),
             RaisedButton(
               onPressed: () {
                 _mapController.animateCamera(CameraUpdate.newCameraPosition(
-                    CameraPosition(target: LatLng(36.314, 52.093),zoom: 6)));
+                    CameraPosition(target: LatLng(36.314, 52.093), zoom: 6)));
               },
               child: Text("♥Go To Iran♥"),
-            )
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 16, bottom: 10),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      FloatingActionButton(
+                        onPressed: _onMapTypeButtonPressed,
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.map),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      FloatingActionButton(
+                        onPressed: null,
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.add_location),
+                      ),
+                      FloatingActionButton(
+                        onPressed: null,
+                        backgroundColor: Colors.green,
+                        child: Icon(Icons.remove),
+                      ),
+                    ],
+                  ),
+                )),
           ],
         ));
+  }
+
+  void _myAddMarker(LatLng point) {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId("marker8"),
+          infoWindow: InfoWindow(
+            title: 'My position is $point',
+          ),
+          draggable: true,
+          position: point,
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueYellow,
+          )));
+    });
   }
 }
